641ddfecce5b2514e8610b286606635030119969
ptbw <ptbwillis@gmail.com>
2016-08-25 21:48:47 +0100

    Menu event handlers

---
+++ Fritz/aboutbox.cpp
@@ -0,0 +1,6 @@
+#include "aboutbox.h"
+
+aboutbox::aboutbox(QWidget *parent) :
+    QMainWindow(parent)
+{
+}
---
+++ Fritz/aboutbox.h
@@ -0,0 +1,18 @@
+#ifndef ABOUTBOX_H
+#define ABOUTBOX_H
+
+#include <QMainWindow>
+
+class aboutbox : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit aboutbox(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // ABOUTBOX_H
